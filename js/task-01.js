26eb1e6ff12b99698e7e55ed3a4a56324f19be4e
Yaroslav Kulyk <kulyk.yaroslav@gmail.com>
2021-06-16 20:01:58 +0300

    Сделал задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
-const categoriesRef = document.querySelectorAll('.item');
+const categoriesRef = document.querySelectorAll('#categories .item');
 
 console.log(`В списке ${categoriesRef.length} категории.`);
+
+const categoriesDescription = categoriesRef.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryEntries = category.querySelector('ul').children.length;
+  console.log(
+    `Категория: ${categoryName}\nКоличество элементов: ${categoryEntries}`,
+  );
+}, '');
